4403e2d37e1baa9f4e73df416662f998444c37ea
Priyanka M <priyankapriyanka94642@gmail.com>
2026-02-09 10:11:30 +0530

    Time: 1 ms (29.61%) | Memory: 8.8 MB (95.75%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i,j,carry=0,bitA,bitB,sum;
+        string res = "";
+        i=a.size()-1;
+        j=b.size()-1;
+        carry=0;
+        while(i>=0||j>=0||carry){
+            if(i>=0){
+                bitA=a[i--]-'0';
+            }
+            else{
+                bitA=0;
+            }
+            if(j>=0){
+                bitB=b[j--]-'0';
+            }
+            else{
+                bitB=0;
+            }
+            sum=bitA+bitB+carry;
+            res.push_back((sum%2)+'0');
+            carry=sum/2;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
