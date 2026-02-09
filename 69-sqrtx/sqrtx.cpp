c93ab00c46d4bca5c11f22efb6db753d6636840b
Priyanka M <priyankapriyanka94642@gmail.com>
2026-02-09 10:44:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.26%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long i = 0, j = x;
+        int ans = 0;
+
+        while(i <= j){
+            long long mid = i + (j - i) / 2;
+
+            if(mid * mid <= x){
+                ans = mid;
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
