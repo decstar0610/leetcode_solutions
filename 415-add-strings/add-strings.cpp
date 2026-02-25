f828114e5db34cbb107fe2015bf28a9c6d4002dd
Priyanka M <priyankapriyanka94642@gmail.com>
2026-02-25 17:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (58.60%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+
+        int n1 = num1.size() - 1;
+        int n2 = num2.size() - 1;
+
+        int carry = 0;
+        string ans = "";
+
+        while(n1 >= 0 || n2 >= 0 || carry){
+
+            int sum = carry;
+
+            if(n1 >= 0){
+                sum += num1[n1] - '0';
+                n1--;
+            }
+
+            if(n2 >= 0){
+                sum += num2[n2] - '0';
+                n2--;
+            }
+             
+            ans.push_back((sum % 10) + '0');
+            carry = sum / 10;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
